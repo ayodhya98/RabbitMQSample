7f0d566c0b4f915fafb2f00e6f12ae0e05036a5f
Ayodhya <ayodhyadeshad22@gmail.com>
2025-07-29 12:18:01 +0530

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare
+    (
+    queue: "ayodhya-queue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[Consumer] Received: {message}");
+};
+
+channel.BasicConsume
+    (
+    queue: "ayodhya-queue",
+    autoAck: true,
+    consumer: consumer
+    );
+
+Console.WriteLine("Waiting for messages.....");
+Console.ReadLine();
---
+++ Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() 
+{
+HostName = "localhost",
+};
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare
+    (
+        queue: "ayodhya-queue",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+
+Console.WriteLine("What would you like to send? ");
+string? message = Console.ReadLine();
+var body = Encoding.UTF8.GetBytes(message?? "Default msg");
+
+channel.BasicPublish
+    (
+    exchange: "",
+    routingKey: "ayodhya-queue",
+    basicProperties: null,
+    body: body
+    );
+
+Console.WriteLine($"[Ayodhya] Sent: {message}");
